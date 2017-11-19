05f565d24d855a56c73651377b9c0b6689203c04
Ivan <skripachevivan@gmail.com>
2017-11-20 01:13:21 +0300

    Second commit

--- lab1/factorial/factorial/Source.cpp
+++ lab1/factorial/factorial/Source.cpp
@@ -5,3 +5,8 @@ using namespace std;
 int f_factor(int num) {
 	return (num != 1) ? (f_factor(num - 1)*num) : 1;
 }
+
+int main(){
+	#Test with input 5 and output 120
+	if(f_factor(5) == 120) cout<<"Ok"<<endl;
+}
