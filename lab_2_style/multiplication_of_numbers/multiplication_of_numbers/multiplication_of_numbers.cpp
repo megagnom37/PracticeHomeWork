310a4b39eb227d15dc97cea01187a028130ce328
megagnom37 <skripachevivan@gmail.com>
2017-12-04 20:46:27 +0300

    added new multiplication_of_numbers.cpp

--- lab_2_style/multiplication_of_numbers/multiplication_of_numbers/multiplication_of_numbers.cpp
+++ lab_2_style/multiplication_of_numbers/multiplication_of_numbers/multiplication_of_numbers.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 
 long long MulOfNumbers(long long aNumber) {
-  if (aNumber == 0) {
-		return 10;
-	} else if (aNumber == 1) {
-		return 1;
-	}
-	//
-	long long q(0);
-	long long multiplier(1);
-	//
-	for (long long i(9); i > 1; --i) {
-		while ((aNumber % i) == 0) {
-			q = q + (multiplier * i);
-			multiplier = multiplier * 10;
-			aNumber = aNumber / i;
-		}
-	}
-	//
-	return (aNumber == 1) ? q : -1;
+  if(aNumber == 0) {
+    return 10;
+  } else if(aNumber == 1) {
+    return 1;
+  }
+  //
+  long long q(0);
+  long long multiplier(1);
+  //
+  for(long long i(9); i > 1; --i) {
+    while((aNumber % i) == 0) {
+      q = q + (multiplier * i);
+      multiplier = multiplier * 10;
+      aNumber = aNumber / i;
+    }
+  }
+  //
+  return (aNumber == 1) ? q : -1;
 }
 
 int main() {
-	long long n;
-	std::cin >> n;
-	std::cout << MulOfNumbers(n) << std::endl;
-	return 0;
+  long long n;
+  std::cin >> n;
+  std::cout << MulOfNumbers(n) << std::endl;
+  return 0;
 }
